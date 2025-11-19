ed8aecc2765ab679e3bd62ca3e2501e5957102a2
jpfree <jpfree@users.noreply.github.com>
2025-11-19 17:01:47 +0800

    add mips

--- scripts/binaries.mk
+++ scripts/binaries.mk
@@ -32,6 +32,10 @@ FROM build-base AS build-linux-arm64
 RUN GOOS=linux GOARCH=arm64 go build -ldflags "-X main.version=$$VERSION" -o tmp/$(BINARY_NAME)
 RUN tar -C tmp -czf binaries/$(BINARY_NAME)_$${VERSION}_linux_arm64v8.tar.gz --owner=0 --group=0 $(BINARY_NAME)
 
+FROM build-base AS build-linux-mips
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=mips GOMIPS=softfloat go build -ldflags "-X main.version=$$VERSION" -o tmp/$(BINARY_NAME)
+RUN tar -C tmp -czf binaries/$(BINARY_NAME)_$${VERSION}_linux_mips.tar.gz --owner=0 --group=0 $(BINARY_NAME)
+
 FROM $(BASE_IMAGE)
 COPY --from=build-windows-amd64 /s/binaries /s/binaries
 COPY --from=build-linux-amd64 /s/binaries /s/binaries
